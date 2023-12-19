7b5e449c50a971577f9227dd305db8530607faae
shivashankarmn <131743146+shivashankarmn@users.noreply.github.com>
2023-12-20 02:47:49 +0530

    Delete game/math-game-master directory

--- game/math-game-master/index.js
+++
@@ -1,7 +0,0 @@
-const app = require('./src/app');
-require('./src/database/database');
-
-app.listen(app.set('port'));
-console.log('server running on port '+ app.set('port'))
-
-module.exports = app;
--- game/math-game-master/src/middleware/error.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(err, req, res, next) {
-
-    //log the exception
-    console.log(err.message);
-
-    res.status(500).send('Something Failed. Try Again');    
-}
